22ef1160228669d016782441053ca78df1b9859a
Ｎathan Lu <nathan.lu@tkspring.com>
2024-04-26 16:54:46 +0800

    feat(api): add

--- internal/api/router.go
+++ internal/api/router.go
@@ -6,7 +6,7 @@ func SetupRouter(engine *gin.Engine) {
 	//h := NewHandler()
 
 	//v1 := engine.Group("/v1", middleware.AuthMiddleware(OAUTH_STATE_STRING), middleware.FunctionPermissionAuthzMiddleware())
-	v1 := engine.Group("/v1")
-	v1.POST("/coupons/reservations", h.ReserveCoupon)
-	v1.POST("/coupons/purchases", h.PurchaseCoupon)
+	//v1 := engine.Group("/v1")
+	//v1.POST("/coupons/reservations", h.ReserveCoupon)
+	//v1.POST("/coupons/purchases", h.PurchaseCoupon)
 }
